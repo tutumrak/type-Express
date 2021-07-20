fd7522dd950f33bfde90ccc1696a7a3e0eeff3aa
Lazar Tutulic <tutuliclazar0@gmail.com>
2021-07-20 13:42:07 +0200

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,11 @@ import Controller from './interfaces/controller.interface';
 import errorHandler from './middleware/errorhandler';
 
 export class App {
-    private port: number;
     private app: express.Application;
     constructor(port: number, controllers: Controller[]) {
-        this.port = port;
         this.app = express();
-        this.connectToDatabase()
-            .then(() => this.app.listen(this.port))
-            .catch((err) => console.log(err));
-
+        this.connectToDatabase().then(() => console.log(`Succesfully connected to db`)).catch((err) => console.log(err));
+        this.listen(port);
         this.initializeMiddleware();
         this.initializeRoutes(controllers);
         this.initializeErrorHandlers();
@@ -36,8 +32,10 @@ export class App {
             MONGO_PATH
         } = process.env;
         return mongoose.connect(
-            `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, { useUnifiedTopology: true, useNewUrlParser: true })
-        
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`, { useUnifiedTopology: true, useNewUrlParser: true })
+    }
+    public listen(port: number){
+        return this.app.listen(port);
     }
     
 }
